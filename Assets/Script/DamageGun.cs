98078ae6ef7fd3535c48c1e0b7fa9253ec0b66ba
hafizfikry <140366914+hafizfikry@users.noreply.github.com>
2024-02-20 19:00:14 +0700

    Ghsot

--- Assets/Script/DamageGun.cs
+++ Assets/Script/DamageGun.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml;
 using UnityEngine;
 
 public class DamageGun : MonoBehaviour
 {
     public float damageGun;
     public float BulletRange;
-    private Transform PlayerCamera;
+    public GameObject particleEffect; // Particle effect object to be assigned in the inspector
+    private Transform playerTransform; // Player's transform
+
     // Start is called before the first frame update
     void Start()
     {
-        PlayerCamera = Camera.main.transform;
+        // Assuming player is the parent object, otherwise adjust accordingly
+        playerTransform = transform.parent; // Assuming the player is the parent object
     }
 
     // Update is called once per frame
     public void shoot()
     {
-        Ray gunRay = new Ray(PlayerCamera.position, PlayerCamera.forward);
-        if(Physics.Raycast(gunRay,out RaycastHit hitInfo,BulletRange))
+        Ray gunRay = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
         {
-            if(hitInfo.collider.gameObject.TryGetComponent(out Entity enemy))
+            if (hitInfo.collider.gameObject.TryGetComponent(out Entity enemy))
             {
                 enemy.Health -= 1;
             }
+
+            // Spawn particle effect from player's position
+            Instantiate(particleEffect, playerTransform.position, Quaternion.identity);
         }
     }
 }
