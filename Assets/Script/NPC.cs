5b3fdbbe92f65c4642246fd76c2e143bac3ede3b
User-Han <142723546+UserHan9@users.noreply.github.com>
2024-05-28 20:41:43 +0700

    anjay

--- Assets/Script/NPC.cs
+++ Assets/Script/NPC.cs
@@ -4,11 +4,19 @@ using System.Collections;
 
 public class NPC : MonoBehaviour
 {
-    
+    // Referensi ke canvas dialogue
     public GameObject dialogueCanvas;
+
+    // Referensi ke TextMeshProUGUI di dalam canvas dialogue
     public TextMeshProUGUI dialogueText;
+
+    // Array untuk menyimpan berbagai dialog
     public string[] dialogues;
+
+    // Kecepatan penulisan teks
     public float textSpeed = 0.05f;
+
+    // Coroutine untuk menampilkan teks satu per satu
     private Coroutine typingCoroutine;
 
     private int currentDialogueIndex = 0; // Indeks dialog saat ini
@@ -31,7 +39,7 @@ public class NPC : MonoBehaviour
     private void Update()
     {
         // Memeriksa jika teks telah selesai ditampilkan dan tombol kanan mouse ditekan
-        if (isDialogActive && Input.GetMouseButtonDown(0) && !IsTyping())
+        if (isDialogActive && Input.GetMouseButtonDown(1) && !IsTyping())
         {
             ShowNextDialogue();
         }
@@ -68,6 +76,7 @@ public class NPC : MonoBehaviour
         typingCoroutine = null;
     }
 
+    // Method untuk menyembunyikan canvas dialogue
     public void HideDialogueCanvas()
     {
         if (dialogueCanvas != null)
